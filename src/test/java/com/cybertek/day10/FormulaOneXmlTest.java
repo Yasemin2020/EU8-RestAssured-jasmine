2b35fb75b263ec98e5dad299b139357f7b2cbef9
Jasmine <bayramali5219@gmail.com>
2022-07-30 12:45:36 +0300

    Day 10 Formula xml attribute

--- src/test/java/com/cybertek/day10/FormulaOneXmlTest.java
+++ src/test/java/com/cybertek/day10/FormulaOneXmlTest.java
@@ -1,5 +1,8 @@
 package com.cybertek.day10;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,8 +11,43 @@ import static org.hamcrest.Matchers.*;
 public class FormulaOneXmlTest {
     @BeforeAll
     public static void setUp(){
+        //http://ergast.com/api/f1/drivers/alonso
         baseURI="https://ergast.com/";
+        basePath="/api/f1";
+
+
     }
+    @Test
+    public void test1(){
+        Response response = given()
+                                     .pathParam("driver", "alonso")
+                            .when()
+                                     .get("/drivers/{driver}")
+                            .then()
+                                    .statusCode(200).log().all()
+                                    .extract().response();
+
+        XmlPath xmlPath = response.xmlPath();
+        //get given name
+        String givenName = xmlPath.getString("MRData.DriverTable.Driver.GivenName");
+        System.out.println("givenName = " + givenName);
+        //get familyName
+        String familyName = xmlPath.getString("MRData.DriverTable.Driver.FamilyName");
+        System.out.println("familyName = " + familyName);
+        //get driver ID
+        String driverID = xmlPath.getString("MRData.DriverTable.Driver.@driverId");
+        System.out.println("driverID = " + driverID);
+        //get code
+        String code = xmlPath.getString("MRData.DriverTable.Driver.@code");
+        System.out.println("code = " + code);
+        //getURL
+        String url = xmlPath.getString("MRData.DriverTable.Driver.@url");
+        System.out.println("url = " + url);
+
+
+    }
+
+
 
 
 }
